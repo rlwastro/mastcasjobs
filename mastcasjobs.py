e1c164014ed9a46ad46d821df098d6ad76901c35
rlwastro <rlw@stsci.edu>
2019-04-17 13:18:35 -0400

    mastcasjobs.py: add code for username login and drop_table_if_exists requirements.txt: add requests setup.py: bump version to 0.0.2

--- mastcasjobs.py
+++ mastcasjobs.py
@@ -5,6 +5,8 @@ vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 :
 """
 
 from casjobs import CasJobs
+import os, requests
+import xml.etree.ElementTree as ET
 
 __all__ = ["MastCasJobs", "contexts"]
 
@@ -16,10 +18,12 @@ contexts = [
             "GALEX_UV_BKGD",
             "HLSP_47Tuc",
             "HSLP_GSWLC",
+            "HSCv3",
             "HSCv2",
             "HSCv1",
             "Kepler",
             "PanSTARRS_DR1",
+            "PanSTARRS_DR2",
             "PHATv2",
             "SDSS_DR12",
             ]
@@ -30,12 +34,18 @@ class MastCasJobs(CasJobs):
 
     ## Keyword Arguments
 
-    * `userid` (int): The WSID from your CasJobs profile. If this is not
-      provided, it should be in your environment variable `CASJOBS_WSID`.
+    * `username` (str): Your CasJobs user name. It can also be
+      provided by the `CASJOBS_USERID` environment variable.
     * `password` (str): Your super-secret CasJobs password. It can also be
       provided by the `CASJOBS_PW` environment variable.
+    * `userid` (int): The WSID from your CasJobs profile. This is not used
+      if `username` is specified.  Note that when this alternate login method is
+      used, the get_fast_table method will not work.  This can also be
+      provided by the `CASJOBS_WSID` environment variable.
     * `base_url` (str): The base URL that you'd like to use depending on the
       service that you're accessing. Default is the MAST Casjobs URL.
+    * `wsid_url` (str): The service URL that is used to login using the username
+      and password.  Default is the MAST Casjobs service URL.
     * `request_type` (str): The type of HTTP request to use to access the
       CasJobs services.  Can be either 'GET' or 'POST'.  Typically you
       may as well stick with 'GET', unless you want to submit some long
@@ -44,9 +54,28 @@ class MastCasJobs(CasJobs):
     * `context` (str): Default context that is used for queries.
 
     """
-    def __init__(self, userid=None, password=None,
+    def __init__(self, username=None, password=None, userid=None,
                  base_url="http://mastweb.stsci.edu/ps1casjobs/services/jobs.asmx",
+                 wsid_url="https://mastweb.stsci.edu/ps1casjobs/casusers.asmx/GetWebServiceId",
                  request_type="GET", context="PanSTARRS_DR1"):
+
+        # order of preference: (1) username (2) userid (3) CASJOBS_USERID (4) CASJOBS_WSID (5) prompt
+        if username is None:
+            if userid is None:
+                username = os.environ.get('CASJOBS_USERID')
+                if username is None:
+                    userid = os.environ.get('CASJOBS_WSID')
+        if username is None and userid is None:
+            raise ValueError("Specify username or set CASJOBS_USERID environment variable")
+        if password is None:
+            password = os.environ.get('CASJOBS_PW')
+            if password is None:
+                raise ValueError("Specify password or set CASJOBS_PW environment variable")
+        self.username = username
+        self.wsid_url = wsid_url
+        self.password = password
+        if username is not None:
+            userid = self.get_wsid()
 
         super(MastCasJobs,self).__init__(userid=userid, password=password,
                 base_url=base_url, request_type=request_type, context=context)
@@ -64,3 +93,31 @@ class MastCasJobs(CasJobs):
         # the first line is a header and the last is always empty
         # also, the table names have " as the first and last characters
         return [l[1:-1]for l in res.split('\n')[1:-1]]
+
+    def drop_table_if_exists(self, table):
+        """
+        Drop table from MyDB without an error if it does not exist
+
+        ## Arguments
+
+        * `table` (str): The name of the table to drop.
+        """   
+        results = self.quick("DROP TABLE IF EXISTS {}".format(table),context="MYDB")
+
+    def get_wsid(self):
+
+        """Returns WSID for this Casjobs username and password
+        
+        ## Returns
+
+        * `wsid` (str): The WSID for the user
+        """   
+
+        r = requests.post(self.wsid_url, data=dict(userid=self.username, password=self.password))
+        r.raise_for_status()
+        root = ET.fromstring(r.text)
+        if root is None or (not root.text) or root.text == "-1":
+            raise ValueError("Incorrect MAST Casjobs username/password")
+        else:
+            return root.text.strip()
+
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="mastcasjobs",
-    version="0.0.1",
+    version="0.0.2",
     author="Richard L. White",
     author_email="rlw@stsci.edu",
     url="https://github.com/rlwastro/mastcasjobs",
